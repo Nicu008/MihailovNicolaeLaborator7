8effb282cb0028a686c030738cf6560de9ae8dac
Nicolae <nicumihailovtic@gmail.com>
2024-11-28 01:11:49 +0200

    Lab8

---
+++ ListPage.xaml.cs
@@ -0,0 +1,24 @@
+using MihailovNicolaeLaborator7.Models;
+namespace MihailovNicolaeLaborator7;
+using SQLite;
+
+public partial class ListPage : ContentPage
+{
+	public ListPage()
+	{
+		InitializeComponent();
+	}
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        await App.Database.DeleteShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+}
