c9f6bcaa6bd7c22cc6acd13fa93f4110c0f83f3d
Nicolae <nicumihailovtic@gmail.com>
2024-12-05 21:39:09 +0200

    lab9

---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+using MihailovNicolaeLaborator7.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MihailovNicolaeLaborator7
+{
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        [OneToMany]
+        public List <ListProduct> ListProducts { get; set; }
+    }
+}
